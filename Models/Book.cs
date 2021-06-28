85841a14b04ed04c5d886f0143bb16ee72d46d97
Freddy Rosembergh Mendoza Tovar <fr_mendoza@misena.edu.co>
2021-06-28 11:54:27 -0500

    Add .gitignore and .gitattributes.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace editorial.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName{ get; set; }
+        public string Price{ get; set; }
+        public string Category{ get; set; }
+        public string Author{ get; set; }
+    }
+}
